6fc02fde7eb3e26b7ca28f8b2a0f9efea8bc2dfc
lipitos <fneves.si@gmail.com>
2021-09-23 16:51:04 -0300

    Criação de tratamento de exceção

--- backend/src/main/java/com/fneves/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/fneves/dscatalog/services/CategoryService.java
@@ -3,11 +3,13 @@ package com.fneves.dscatalog.services;
 import com.fneves.dscatalog.dto.CategoryDTO;
 import com.fneves.dscatalog.entities.Category;
 import com.fneves.dscatalog.repositories.CategoryRepository;
+import com.fneves.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,12 +21,15 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public List<CategoryDTO> findAll(){
         List<Category> list = repository.findAll();
-
         //Expresão Lambda
         List<CategoryDTO> listDto = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-
         return listDto;
     }
 
-
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category categoryDto = obj.orElseThrow(() -> new EntityNotFoundException("Categoria não encontrada."));
+        return new CategoryDTO(categoryDto);
+    }
 }
