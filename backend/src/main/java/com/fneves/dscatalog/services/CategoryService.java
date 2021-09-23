11f646c00a94f4156aab728455e853fe996fcdbc
lipitos <fneves.si@gmail.com>
2021-09-23 08:24:24 -0300

    Criação do Service e Repository. Configuração do Banco H2. Inclusão da primeira category via banco

---
+++ backend/src/main/java/com/fneves/dscatalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.fneves.dscatalog.services;
+
+import com.fneves.dscatalog.entities.Category;
+import com.fneves.dscatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+
+}
