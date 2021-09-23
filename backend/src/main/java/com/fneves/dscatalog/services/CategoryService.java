042346087b80e28ab247e5602d1d53976da51a84
lipitos <fneves.si@gmail.com>
2021-09-23 11:55:27 -0300

    Database seeding

--- backend/src/main/java/com/fneves/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/fneves/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.fneves.dscatalog.entities.Category;
 import com.fneves.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,8 +14,10 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
         return repository.findAll();
     }
+
 
 }
