aa126bf7e5ed95c6f8915b3f878ccebfdafe72fe
lipitos <fneves.si@gmail.com>
2021-09-23 12:08:49 -0300

    Criação do DTO

--- backend/src/main/java/com/fneves/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/fneves/dscatalog/services/CategoryService.java
@@ -1,5 +1,6 @@
 package com.fneves.dscatalog.services;
 
+import com.fneves.dscatalog.dto.CategoryDTO;
 import com.fneves.dscatalog.entities.Category;
 import com.fneves.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -15,8 +17,13 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<Category> findAll(){
-        return repository.findAll();
+    public List<CategoryDTO> findAll(){
+        List<Category> list = repository.findAll();
+
+        //Expresão Lambda
+        List<CategoryDTO> listDto = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
+        return listDto;
     }
 
 
